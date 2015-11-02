ab619765cab6c636ef53c33a1057de802c958471
beni.buff <beni.buff@gmail.com>
2015-11-02 10:23:59 +0100

    bookingTest.java

---
+++ src/test/java/org/javaee7/movieplex7/booking/BookingTest.java
@@ -0,0 +1,14 @@
+package org.javaee7.movieplex7.booking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
